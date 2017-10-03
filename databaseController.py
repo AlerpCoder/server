3ee0d54a7c20ac866c47a7c2722ddd8e5f2b62a2
stcsteinrueck <IeY4eeyoo1qu>
2017-10-03 19:34:50 +0200

    finished the Post and get method

--- databaseController.py
+++ databaseController.py
@@ -1,7 +1,17 @@
 import os, sys, sqlite3
 
-connection = sqlite3.connect("./tempData.db")
-cursor = connection.cursor()
 
-#create database
-cursor.execute('''create table if not exists running (date text, temperature float, humidity float)''')
+def createDb():
+    connection = sqlite3.connect("./tempData.db")
+    print("Opened database successfully")
+
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE temp(date TEXT, temperature FLOAT, humidity FLOAT)")
+    print("Table created successfully")
+
+    connection.commit()
+    connection.close()
+
+
+if __name__ == "__main__":
+    createDb()
--- server.py
+++ server.py
@@ -1,37 +1,41 @@
 from flask import Flask, jsonify, abort, make_response, request
+import sqlite3
+from datetime import datetime, date, time
 
 app = Flask(__name__)
 
+
 @app.route('/contents', methods=['GET'])
 def get_temperature():
-    return jsonify({'tasks': tasks})
+    connect = sqlite3.connect("./tempData.db")
+    temps = []
+    for row in connect.execute("select * from temp"):
+        temps.append({"date": row[0], "temperature": row[1], "humidity": row[2]})
+    return jsonify(temps)
 
 
 @app.route('/contents', methods=['POST'])
-def create_tempearture():
-    if not request.json or not 'temperature' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'temp': request.json['title'],
-        'hum': request.json.get('descritpion', ""),
-        'done': False
-    }
-    cursor = sqlite3.connect("./tempData.db").cursor()
-    cursor.execute('''insert into running values('')''')
-
-
-    return jsonify({'task': task}), 201
+def create_temperature():
+    currentDate = datetime.now().strftime('%d.%m.%Y, %H:%m:%s')
+    temp = request.json['temperature']
+    hum = request.json['humidity']
+    connect = sqlite3.connect("./tempData.db")
+    cursor = connect.cursor()
+    cursor.execute("insert into temp values(?,?,?)", (currentDate, temp, hum))
+    connect.commit()
+    connect.close()
+    return 'finished', 201
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': error}), 404)
+
 
 @app.errorhandler(400)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not fetch'}), 404)
+    return make_response(jsonify({'error': error}), 404)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
