14456ece5872d5dfa8e9340a103dab35f5e502a2
stcsteinrueck <IeY4eeyoo1qu>
2017-10-05 21:53:10 +0200

    fixed timebug and added index.html

--- server.py
+++ server.py
@@ -1,27 +1,32 @@
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, render_template, make_response, request
 import sqlite3
-from datetime import datetime, date, time
+import time
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
 
 
 @app.route('/contents', methods=['GET'])
 def get_temperature():
     connect = sqlite3.connect("./tempData.db")
     temps = []
-    for row in connect.execute("select * from temp"):
+    for row in connect.execute("SELECT * FROM temp"):
         temps.append({"date": row[0], "temperature": row[1], "humidity": row[2]})
     return jsonify(temps)
 
 
 @app.route('/contents', methods=['POST'])
 def create_temperature():
-    currentDate = datetime.now().strftime('%d.%m.%Y, %H:%m:%s')
+    currentDate = time.strftime('%d.%m.%Y, %H:%m:%s')
     temp = request.json['temperature']
     hum = request.json['humidity']
     connect = sqlite3.connect("./tempData.db")
     cursor = connect.cursor()
-    cursor.execute("insert into temp values(?,?,?)", (currentDate, temp, hum))
+    cursor.execute("INSERT INTO temp VALUES(?,?,?)", (currentDate, temp, hum))
     connect.commit()
     connect.close()
     return 'finished', 201
@@ -38,4 +43,4 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+    app.run(debug=False)
