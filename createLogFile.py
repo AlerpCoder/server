d93e220fee645fa44be5907f0a72b6a876318a92
stcsteinrueck <IeY4eeyoo1qu>
2017-10-07 14:13:48 +0200

    add grap.js to the index.html

---
+++ createLogFile.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def log():
+    logging.basicConfig(filename='klj.log', level=logging.DEBUG)
+    logging.debug('This message should go to the log file')
+    logging.info('So should this')
+    logging.warning('And this, too')
+
+if __name__ == '__main__':
+    log()
--- server.py
+++ server.py
@@ -1,22 +1,34 @@
-from flask import Flask, jsonify, render_template, make_response, request
+import json
+import logging
 import sqlite3
 import time
+from functools import reduce
+
+from flask import Flask, jsonify, render_template, make_response, request
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
-
-
-@app.route('/contents', methods=['GET'])
-def get_temperature():
+def get_json():
     connect = sqlite3.connect("./tempData.db")
     temps = []
     for row in connect.execute("SELECT * FROM temp"):
         temps.append({"date": row[0], "temperature": row[1], "humidity": row[2]})
-    return jsonify(temps)
+    return temps
+
+
+@app.route('/', methods=['GET'])
+def index():
+    labels = map(lambda x: x['date'], get_json())
+    data_temp = map(lambda x: x['temperature'], get_json())
+    data_hum = map(lambda x: x['humidity'], get_json())
+    return render_template("index.html", labels=map(json.dumps, labels), temp_data=map(json.dumps, data_temp),
+                           hum_data=map(json.dumps, data_hum))
+
+
+@app.route('/contents', methods=['GET'])
+def get_temperature():
+    return jsonify(get_json())
 
 
 @app.route('/contents', methods=['POST'])
@@ -33,14 +45,11 @@ def create_temperature():
 
 
 @app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': error}), 404)
-
-
-@app.errorhandler(400)
-def not_found(error):
-    return make_response(jsonify({'error': error}), 400)
+def not_found(exception):
+    app.logger.error(exception)
+    return make_response(jsonify({'error': request.full_path}), 404)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='server/debug.log', level=logging.DEBUG)
     app.run(host="0.0.0.0", debug=False)
