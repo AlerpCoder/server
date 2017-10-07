6bc0a8a1896d6a3b89b4105b6806d1d1e86b7fc5
stcsteinrueck <IeY4eeyoo1qu>
2017-10-07 14:37:18 +0200

    finished first step for deployment

--- static/js/graph.js
+++ static/js/graph.js
@@ -4,8 +4,8 @@ var myChart = new Chart(ctx, {
     data: {
         labels: date,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Temperature',
+            data: hum_data,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
