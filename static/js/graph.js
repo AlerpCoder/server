e8d32996e4e9b0d24bcf64f453cef27034b6deaf
stcsteinrueck <IeY4eeyoo1qu>
2017-10-07 16:59:03 +0200

    added color to the datesets

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,37 +1,25 @@
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: date,
-        datasets: [{
-            label: 'Temperature',
-            data: hum_data,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+        type: 'line',
+        data: {
+            labels: date,
+            datasets: [
+                {
+                    label: 'Temp',
+                    data: temp_data,
+                    borderColor: "rgba(255,0,0,0.6)",
+                    backgroundColor: "rgba(255,0,0,0.5)",
+                    pointBackgroundColor: "rgba(255,0,0,0.5)",
+                    fill: false
+                },
+                {
+                    label: 'Hum',
+                    data: hum_data,
+                    borderColor: "rgba(0,0,255,0.6)",
+                    backgroundColor: "rgba(0,0,255,0.5)",
+                    pointBackgroundColor: "rgba(0,0,255,0.5)",
+                    fill: false
+                }]
+        },
+    })
+;
