a147e69c248858afbbfb25ae1a1bc2c5d56c5d3b
AlerpCoder <steinrchris@aol.com>
2017-10-07 19:40:43 +0200

    fixed path problem

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ import logging
 import sqlite3
 import time
 
+import os
 from flask import Flask, jsonify, render_template, make_response, request
 
 app = Flask(__name__)
 
 
 def get_json():
-    connect = sqlite3.connect("./tempData.db")
-    print(connect)
-    cur = connect.cursor()
-    print(cur)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "tempData.db")
+    connect = sqlite3.connect(db_path)
     temps = []
     for row in connect.execute("SELECT * FROM temp"):
         temps.append({"date": row[0], "temperature": row[1], "humidity": row[2]})
@@ -35,10 +35,12 @@ def get_temperature():
 
 @app.route('/contents', methods=['POST'])
 def create_temperature():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "tempData.db")
+    connect = sqlite3.connect(db_path)
     currentDate = time.strftime('%d.%m.%Y, %H:%m:%s')
     temp = request.json['temperature']
     hum = request.json['humidity']
-    connect = sqlite3.connect("./tempData.db")
     cursor = connect.cursor()
     cursor.execute("INSERT INTO temp VALUES(?,?,?)", (currentDate, temp, hum))
     connect.commit()
